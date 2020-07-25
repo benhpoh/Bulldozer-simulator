80d1091ba13a35e20d7358849c933f873b9ac9ef
Ben P <ben.h.poh@gmail.com>
2020-07-25 17:34:32 +1000

    Updated to do list, strategy for continuing task

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Bulldozer-simulator
+
+## To Do List
+- ~~Build Bulldozer Class with necessary commands~~
+  - Build calculator to keep tabs on fuel and credits
+- Build functionality to read map.txt
+  - Feed data into "advance" command to categorize simulation ending moves / add to fuel and credit costs
+- Build loop for CLI to receive commands
+- Build quit command. Upon quitting output:
+  - Simulation ended due to (user command / illegal command)
+  - History
+  - Costs
+
+### Build log
+- Basic commands completed (left, right, advance, location, history)
--- test_main.rb
+++ test_main.rb
@@ -6,14 +6,14 @@ Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
 
 class LocationTest < MiniTest::Test
   def test_location_before_commands
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     actual = bulldozer.location
     expected = "X - 0; Y - 1; Facing - East"
     assert_equal(expected, actual)
   end
 
   def test_location_after_advance_left
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     bulldozer.advance(1)
     bulldozer.left
     actual = bulldozer.location
@@ -22,7 +22,7 @@ class LocationTest < MiniTest::Test
   end
 
   def test_location_after_advance_3_right
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     bulldozer.advance(3)
     bulldozer.right
     actual = bulldozer.location
@@ -33,7 +33,7 @@ end
 
 class LeftTest < MiniTest::Test
   def test_left_once
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     bulldozer.left
     actual = bulldozer.location
     expected = "X - 0; Y - 1; Facing - North"
@@ -41,7 +41,7 @@ class LeftTest < MiniTest::Test
   end
 
   def test_left_thrice
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     3.times { bulldozer.left }
     actual = bulldozer.location
     expected = "X - 0; Y - 1; Facing - South"
@@ -49,7 +49,7 @@ class LeftTest < MiniTest::Test
   end
 
   def test_left_four_times
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     4.times { bulldozer.left }
     actual = bulldozer.location
     expected = "X - 0; Y - 1; Facing - East"
@@ -57,7 +57,7 @@ class LeftTest < MiniTest::Test
   end
 
   def test_left_five_times
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     5.times { bulldozer.left }
     actual = bulldozer.location
     expected = "X - 0; Y - 1; Facing - North"
@@ -67,7 +67,7 @@ end
 
 class RightTest < MiniTest::Test
   def test_right_once
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     bulldozer.right
     actual = bulldozer.location
     expected = "X - 0; Y - 1; Facing - South"
@@ -75,7 +75,7 @@ class RightTest < MiniTest::Test
   end
 
   def test_right_thrice
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     3.times { bulldozer.right }
     actual = bulldozer.location
     expected = "X - 0; Y - 1; Facing - North"
@@ -83,7 +83,7 @@ class RightTest < MiniTest::Test
   end
 
   def test_right_four_times
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     4.times { bulldozer.right }
     actual = bulldozer.location
     expected = "X - 0; Y - 1; Facing - East"
@@ -91,7 +91,7 @@ class RightTest < MiniTest::Test
   end
 
   def test_right_five_times
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     5.times { bulldozer.right }
     actual = bulldozer.location
     expected = "X - 0; Y - 1; Facing - South"
@@ -101,14 +101,14 @@ end
 
 class HistoryTest < MiniTest::Test
   def test_history_at_initialization
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     actual = bulldozer.history
     expected = "No commands issued"
     assert_equal(expected, actual)
   end
 
   def test_history_after_left
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     bulldozer.left
     actual = bulldozer.history
     expected = "Turn left"
@@ -116,7 +116,7 @@ class HistoryTest < MiniTest::Test
   end
 
   def test_history_after_left_twice
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     2.times { bulldozer.left }
     actual = bulldozer.history
     expected = "Turn left, Turn left"
@@ -124,7 +124,7 @@ class HistoryTest < MiniTest::Test
   end
 
   def test_history_after_right
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     bulldozer.right
     actual = bulldozer.history
     expected = "Turn right"
@@ -132,7 +132,7 @@ class HistoryTest < MiniTest::Test
   end
 
   def test_history_after_right_twice
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     2.times { bulldozer.right }
     actual = bulldozer.history
     expected = "Turn right, Turn right"
@@ -140,10 +140,21 @@ class HistoryTest < MiniTest::Test
   end
 
   def test_history_after_advance
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     bulldozer.advance(1)
     actual = bulldozer.history
     expected = "Advance 1"
+    assert_equal(expected, actual)
+  end
+
+  def test_history_after_advance_right_advance_left
+    bulldozer = Bulldozer.new
+    bulldozer.advance(1)
+    bulldozer.right
+    bulldozer.advance(3)
+    bulldozer.left
+    actual = bulldozer.history
+    expected = "Advance 1, Turn right, Advance 3, Turn left"
     assert_equal(expected, actual)
   end
 
@@ -151,7 +162,7 @@ end
 
 class AdvanceTest < MiniTest::Test
   def test_advance_without_argument
-    bulldozer = Bulldozer.new()
+    bulldozer = Bulldozer.new
     bulldozer.advance
     actual = bulldozer.location
     expected = "X - 1; Y - 1; Facing - East"
