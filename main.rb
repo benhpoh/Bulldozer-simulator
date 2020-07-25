a46c654557c38939f94fc15c60a0d1740c503fac
Ben P <ben.h.poh@gmail.com>
2020-07-25 16:29:09 +1000

    left and right turn functionality built and tested

--- main.rb
+++ main.rb
@@ -10,4 +10,14 @@ class Bulldozer
   def location()
       "X - #{@position_x}; Y - #{@position_y}; Facing - #{@facing}"
   end
+
+  def left()
+    direction = ["East", "North", "West", "South"]
+    @facing = direction[direction.index(@facing) + 1] || "East"
+  end
+
+  def right()
+    direction = ["East", "North", "West", "South"]
+    @facing = direction[direction.index(@facing) - 1]
+  end
 end
--- test_main.rb
+++ test_main.rb
@@ -1,14 +1,86 @@
 require "minitest/autorun"
 require "minitest/reporters"
+require_relative "main.rb"
 
 Minitest::Reporters.use!(Minitest::Reporters::SpecReporter.new)
 
 class LocationTest < MiniTest::Test
   def test_location_before_commands
-    # assert_equal nil, location()
+    bulldozer = Bulldozer.new()
+    actual = bulldozer.location
+    expected = "X - 0; Y - 1; Facing - East"
+    assert_equal(expected, actual)
   end
 
   def test_location_after_advance_once_command
-    # assert_equal "X - 0; Y - 0; Facing - East", advance(1).location()
+    skip #assert_equal(expected, actual)
+  end
+end
+
+class LeftTest < MiniTest::Test
+  def test_left_once
+    bulldozer = Bulldozer.new()
+    bulldozer.left
+    actual = bulldozer.location
+    expected = "X - 0; Y - 1; Facing - North"
+    assert_equal(expected, actual)
+  end
+
+  def test_left_thrice
+    bulldozer = Bulldozer.new()
+    3.times { bulldozer.left }
+    actual = bulldozer.location
+    expected = "X - 0; Y - 1; Facing - South"
+    assert_equal(expected, actual)
+  end
+
+  def test_left_four_times
+    bulldozer = Bulldozer.new()
+    4.times { bulldozer.left }
+    actual = bulldozer.location
+    expected = "X - 0; Y - 1; Facing - East"
+    assert_equal(expected, actual)
+  end
+
+  def test_left_five_times
+    bulldozer = Bulldozer.new()
+    5.times { bulldozer.left }
+    actual = bulldozer.location
+    expected = "X - 0; Y - 1; Facing - North"
+    assert_equal(expected, actual)
+  end
+end
+
+class RightTest < MiniTest::Test
+  def test_right_once
+    bulldozer = Bulldozer.new()
+    bulldozer.right
+    actual = bulldozer.location
+    expected = "X - 0; Y - 1; Facing - South"
+    assert_equal(expected, actual)
+  end
+
+  def test_right_thrice
+    bulldozer = Bulldozer.new()
+    3.times { bulldozer.right }
+    actual = bulldozer.location
+    expected = "X - 0; Y - 1; Facing - North"
+    assert_equal(expected, actual)
+  end
+
+  def test_right_four_times
+    bulldozer = Bulldozer.new()
+    4.times { bulldozer.right }
+    actual = bulldozer.location
+    expected = "X - 0; Y - 1; Facing - East"
+    assert_equal(expected, actual)
+  end
+
+  def test_right_five_times
+    bulldozer = Bulldozer.new()
+    5.times { bulldozer.right }
+    actual = bulldozer.location
+    expected = "X - 0; Y - 1; Facing - South"
+    assert_equal(expected, actual)
   end
 end
