3119e65ee052210446ed19c36bb1e16f20dd0e16
Ben P <ben.h.poh@gmail.com>
2020-08-07 14:42:13 +1000

    Created new Land class to convert map data from String chars into objects

---
+++ lib/land_type.rb
@@ -0,0 +1,60 @@
+class Land
+  def initialize(map_character)
+    land_types = {
+      "o" => {
+        symbol: :o,
+        type: :Plains,
+        fuel_cost: 1,
+        clearable: true
+      },
+      "r" => {
+        symbol: :r,
+        type: :Rocky,
+        fuel_cost: 2,
+        clearable: true
+      },
+      "t" => {
+        symbol: :t,
+        type: :RemovableTree,
+        fuel_cost: 2,
+        clearable: true
+      },
+      "T" => {
+        symbol: :T,
+        type: :ProtectedTree,
+        fuel_cost: 2,
+        clearable: false
+      },
+      "cleared" => {
+        symbol: :-,
+        type: :Cleared,
+        fuel_cost: 1,
+        clearable: true
+      },
+      "out" => {
+        symbol: nil,
+        type: :OffSite,
+        fuel_cost: nil,
+        clearable: false
+      }
+    }
+
+    @land = land_types[map_character]
+  end
+
+  def symbol
+    @land[:symbol]
+  end
+  
+  def type
+    @land[:type]
+  end
+
+  def clearable
+    @land[:clearable]
+  end
+
+  def cost
+    @land[:fuel_cost]
+  end
+end
--- lib/map.rb
+++ lib/map.rb
@@ -1,46 +1,53 @@
+require_relative "land_type"
+
 class Map
   attr_reader :map_array
-  
+
   def initialize(map_array)
-    @map_array = map_array
+    @map_array = convert_txt_map(map_array)
   end
   
   def clear(x, y)
     x, y = convert_grid_to_index(x, y)
     if can_be_cleared?(x, y)
       square_cleared = @map_array[y][x]
-      @map_array[y][x] = "-"
+      @map_array[y][x] = Land.new("cleared")
 
       square_cleared
       # Returns type of square that was cleared
 
     else # Attempted to clear illegal square
-      return "OUT" if @map_array[y].nil?
+      return Land.new("out") if @map_array[y].nil? || @map_array[y][x].nil?
 
-      @map_array[y][x] == "T" ? "T" : "OUT"
+      @map_array[y][x]
       # Returns type of illegal square being cleared
     end
   end
 
   def can_be_cleared?(x, y)
-    return false if is_outside_site?(x, y) || is_protected_tree?(x, y)
-    
-    true
+    within_site?(x, y) && @map_array[y][x][:clearable]
   end
 
-  def is_protected_tree?(x, y)
-    @map_array[y][x] == "T"
+  def within_site?(x,y)
+    x >= 0 &&
+    x < @map_array[0].length &&
+    y >= 0 &&
+    y < @map_array.length
   end
+  
+  # def is_protected_tree?(x, y)
+  #   @map_array[y][x] == "T"
+  # end
 
-  def is_outside_site?(x,y)
-    if x < 0 || x >= @map_array[0].length
-      true
-    elsif y < 0 || y >= @map_array.length
-      true
-    else
-      false
-    end
-  end
+  # def is_outside_site?(x,y)
+  #   if x < 0 || x >= @map_array[0].length
+  #     true
+  #   elsif y < 0 || y >= @map_array.length
+  #     true
+  #   else
+  #     false
+  #   end
+  # end
 
   def convert_grid_to_index(x, y)
     # programming index starts at 0, 0
@@ -49,6 +56,15 @@ class Map
   end
 
   def display
-    @map_array.map { |row| row.join(" ") }
+    @map_array.map do |row| 
+      row.map { |land| land[:symbol] }
+        .join(" ") 
+    end
+  end
+
+  def convert_txt_map(string_2d_array)
+    string_2d_array.each do |string_array|
+      string_array.map! {|char| Land.new(char) }
+    end
   end
 end
